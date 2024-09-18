b24b1348a451543017d87c5d183a3b1cb96d142b
Jcy857 <chengkanpang@gmail.com>
2024-09-19 00:44:05 +0800

    v1.3.3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let csvData = [];
 
-// 加載 CSV 文件
+// Load csv
 fetch('data.csv')
     .then(response => response.text())
     .then(text => {
@@ -16,19 +16,25 @@ function parseCSV(text) {
 }
 
 function searchCSV() {
-    const keyword_x = document.getElementById('searchInputX').value;
-    const keyword_y = document.getElementById('searchInputY').value;
+    const year = document.getElementById('searchInputX').value;
+    const question = document.getElementById('searchInputY').value;
     const resultTable = document.getElementById('resultTable');
     const tbody = resultTable.querySelector('tbody');
     tbody.innerHTML = '';
-
-    
-    
-    year = String(keyword_x);
-    question = String(keyword_y);
     line = String(csvData[question]);
     ans = line.split(',');
     output = ans[year-1976];
-    tbody.innerHTML = '<tr><td colspan="100%">沒有找到匹配的結果</td></tr>';
-    tbody.innerHTML = '<tr><td colspan="100%">' + year + "年第" + question + "題答案: " + output + "</td></tr>";
+    
+    if (year < 2012){
+        exam_type = "HKCEE"
+    } else {
+        exam_type = "HKDSE"
+    }
+
+    if (["A", "B", "C", "d"].includes(output)){
+        tbody.innerHTML = '<tr><td colspan="100%">'+ exam_type + " " + year + "年第" + question + "題答案: " + output + "</td></tr>";
+    } else {
+        tbody.innerHTML = '<tr><td colspan="100%">沒有找到匹配的結果</td></tr>';
+    }
+    
 }
