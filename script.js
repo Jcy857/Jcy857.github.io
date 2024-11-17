36ede4556e0c00dc0ae83c8885d51131e367e68a
Jcy857 <chengkanpang@gmail.com>
2024-11-17 13:12:05 +0800

    v1.3.7

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ function parseCSV(text) {
     return rows.map(row => row.split(','));
 }
 
+function getColumn(data, colIndex) {
+     return data.map(row => row[colIndex]); 
+}
+
 function searchCSV() {
     const year = document.getElementById('searchInputX').value;
     const question = document.getElementById('searchInputY').value;
@@ -38,3 +42,37 @@ function searchCSV() {
     }
     
 }
+
+function check_answer() {
+    const year = document.getElementById('searchInputX').value;
+    const answers = [];
+    const forms = document.querySelectorAll('.ans_box');
+    forms.forEach((form, index) => {
+        const select = form.querySelector('select');
+        const answer = select.value;
+        answers.push(answer); 
+    });
+
+    let number_of_correct_answer = 0; 
+    let number_of_wrong_answer = 0;
+    const yearIndex = year - 1976;
+    const correctAnswersColumn = getColumn(csvData, yearIndex);
+    const correct_answers = String(correctAnswersColumn).split(",").slice(1,46);
+
+    for (let i=0; i<45; i++) {
+        if (answers[i] === correct_answers[i]) {
+            number_of_correct_answer += 1;
+        } else {
+            number_of_wrong_answer += 1;
+        }
+    }
+
+    correct_pc = number_of_correct_answer / 45 * 100;
+    incorrect_pc = number_of_wrong_answer / 45 * 100;
+    alert(`
+        Correct answers: ${number_of_correct_answer}/45\n
+        Incorrect answers: ${number_of_wrong_answer}/45\n
+        Correct percentage: ${correct_pc.toFixed(2)}%\n
+    `);
+}
+
